b6ddaffd0cac8b32ec3b3dcee2f4c0474fada34f
Homer1020 <61599010+Homer1020@users.noreply.github.com>
2023-06-23 23:09:52 -0400

    Se añadió soporte para postgres y se quito para mysql

--- src/config/database.js
+++ src/config/database.js
@@ -4,7 +4,7 @@ const { DB_USER, DB_PASSWORD, DB_NAME, DB_PORT, DB_HOST } = process.env
 const db = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
   host: DB_HOST,
   port: DB_PORT,
-  dialect: 'mysql',
+  dialect: 'postgres',
   logging: false
 });
 
